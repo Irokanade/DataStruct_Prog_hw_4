8cc20fea4f000bc9ec61984544705885e792a0d9
Irokanade <michaeljs.leong@gmail.com>
2022-12-10 23:09:34 +0800

    Initial Commit

---
+++ DataStruct_Prog_hw_4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStruct_Prog_hw_4
+//
+//  Created by Michael Leong on 10/12/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
